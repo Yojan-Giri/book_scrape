75ed032c94c9801241a9ee9695f3bf3f68ffe284
Yojan-Giri <132761516+Yojan-Giri@users.noreply.github.com>
2025-02-09 14:51:07 +0100

    Update books_scrape.py
    making small changes

--- books_scrape.py
+++ books_scrape.py
@@ -1,56 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[30]:
-
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+books = []
 
-# In[28]:
-
-
-books=[]
+# Scrape 50 pages of books
 for i in range(1, 51):
-    url=f"https://books.toscrape.com/catalogue/page-{i}.html"
-    response=requests.get(url)
-    response=response.content
-    soup=BeautifulSoup(response,'html.parser')
-    ol= soup.find('ol')
-    articles=soup.find_all('article', class_='product_pod')
-    
+    url = f"https://books.toscrape.com/catalogue/page-{i}.html"
+    response = requests.get(url).content
+    soup = BeautifulSoup(response, 'html.parser')
+
+    articles = soup.find_all('article', class_='product_pod')
+
     for article in articles:
-        image= article.find("img")
-        title=image.attrs['alt']
-        star= article.find('p')
-        star = star['class'][1]
-        price=article.find('p', class_="price_color").text
-        price = float(price[1:])
+        image = article.find("img")
+        title = image.attrs['alt']
+        star = article.find('p')['class'][1]  # Extract star rating class
+        price = float(article.find('p', class_="price_color").text[1:])  # Remove £ symbol
         stock_status = article.find('p', class_='instock availability').get_text(strip=True)
-        in_stock =stock_status.strip().lower() == "in stock"
-        
-            
-        
-   
-        
-        books.append([title, price,star, in_stock])
-        
-        
-    
-    
-    
+        in_stock = stock_status.lower() == "in stock"
 
+        books.append([title, price, star, in_stock])
 
-# In[31]:
-
-
-df=pd.DataFrame(books, columns=['Title','Price', 'Star Rating','In Stock'])
-
-
-# In[32]:
-
-
-df.to_csv('books.csv')
-
+# Save to CSV
+df = pd.DataFrame(books, columns=['Title', 'Price', 'Star Rating', 'In Stock'])
+df.to_csv('books.csv', index=False)
